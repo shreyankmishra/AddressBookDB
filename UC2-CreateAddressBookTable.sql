3b27b58bace214465c958f43321226301bd63d52
shreyankmishra <mishra.shreyank@gmail.com>
2020-11-01 23:04:32 +0530

    Add: Creating table AddressBook

---
+++ UC2-CreateAddressBookTable.sql
@@ -0,0 +1,10 @@
+create table AddressBook
+(
+    first_name varchar(20) not null,
+    last_name varchar(20) not null,
+    address varchar(50) not null,
+    city varchar(20) not null,
+    state varchar(20) not null,
+    zip int not null,
+    phone_number varchar(10) not null
+);
