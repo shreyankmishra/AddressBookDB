d836fa7a1c9290dff6409015e5478fd59fd547a1
shreyankmishra <mishra.shreyank@gmail.com>
2020-11-01 23:17:17 +0530

    Add: Inserting new contacts in AddressBook

---
+++ UC3-InsertingNewContacts.sql
@@ -0,0 +1,6 @@
+insert into AddressBook values
+('Sandeep','M','C1 Colony','Satna','MP',526478,'9547862478'),
+('Shobhit','D','C2 Colony','Agra','UP',547525,'8745214789'),
+('Mohit','P','C3 Colony','Sagar','MP',859647,'7858965478'),
+('Kaush','S','C4 Colony','Bhilai','CG',856874,'9852065478'),
+('Akhil','P','C5 Colony','Kanpur','UP',885201,'8520654789')
